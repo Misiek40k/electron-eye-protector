58ae92aaa2f55902f98f4ae91e653f328a3a3ff4
Misiek40k <comanderrr@gmail.com>
2020-03-24 20:40:57 +0100

    Add stopTimer, playBell and closeApp methods

--- app/script.js
+++ app/script.js
@@ -44,6 +44,23 @@ class App extends React.Component {
     });
   };
 
+  stopTimer = () => {
+    clearInterval(this.state.timer);
+    this.setState({
+      status: 'off',
+      time: 0,
+    });
+  };
+
+  playBell = () => {
+    const bell = new Audio('./sounds/bell.wav');
+    bell.play();
+  };
+
+  closeApp = () => {
+    window.close();
+  };
+
 
   render() {
     const { status, time, timer } = this.state;
@@ -51,40 +68,28 @@ class App extends React.Component {
     return (
       <div>
         <h1>Protect your eyes</h1>
-        {status === 'off' && (
-          <div>
-            <p>
-              According to optometrists in order to save your eyes, you should
-              follow the 20/20/20. It means you should to rest your eyes every
-              20 minutes for 20 seconds by looking more than 20 feet away.
-            </p>
-            <p>
-              This app will help you track your time and inform you when it's
-              time to rest.
-            </p>
-          </div>
-        )}
-        {status === 'work' && <img src='./images/work.png' />}
-        {status === 'rest' && <img src='./images/rest.png' />}
-        {status !== 'off' && (
-          <div className='timer'>{this.formatTime(time)}</div>
-        )}
-        {status === 'off' && (
-          <button className='btn' onClick={this.startTimer}>
-            Start
-          </button>
-        )}
-        {status !== 'off' && (
-          <button className='btn' onClick={this.stopTimer}>
-            Stop
-          </button>
-        )}
-        <button className='btn btn-close' onClick={this.closeApp}>
-          X
-        </button>
+
+        {status === 'off' &&
+          <div className="description">
+            <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
+            <p>This app will help you track your time and inform you when it's time to rest.</p>
+          </div>}
+
+        {status === 'work' && <img src="./images/work.png" />}
+
+        {status === 'rest' && <img src="./images/rest.png" />}
+
+        {status !== 'off' && <div className="timer">{this.formatTime(time)}</div>}
+
+        {status === 'off' && <button className="btn" onClick={() => this.startTimer()}>Start</button>}
+
+        {status !== 'off' && <button className="btn" onClick={() => this.stopTimer()}>Stop</button>}
+
+        <button className="btn btn-close" onClick={() => this.closeApp()}>X</button>
       </div>
-    );
+    )
   }
-}
+};
+
 
 render(<App />, document.querySelector('#app'));
