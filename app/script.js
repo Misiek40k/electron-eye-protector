653f55fb7e2bbba83ae71319f67bb0d9aff94226
Misiek40k <comanderrr@gmail.com>
2020-03-24 20:13:59 +0100

    Add formatTime method

--- app/script.js
+++ app/script.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Description } from './Description/Description';
 
 class App extends React.Component {
 
+  formatTime = time => {
+    const minutes = Math.floor(time / 60).toString();
+    const seconds = Math.floor(time % 60).toString();
+
+    return minutes.padStart(2, '0') + ':' + seconds.padStart(2, '0');
+  };
+  
   render() {
+
+    const { status } = this.state;
+
     return (
       <div>
         <h1>Protect your eyes</h1>
-        <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
-        <p>This app will help you track your time and inform you when it's time to rest.</p>
-        <img src="./images/work.png" />
-        <img src="./images/rest.png" />
-        <div className="timer">
-          18:23
-        </div>
-        <button className="btn">Start</button>
-        <button className="btn">Stop</button>
+        {(status === 'off') && <Description />}
+        {(status === 'work') && <img src="./images/work.png" />}
+        {(status === 'rest') && <img src="./images/rest.png" />}
+        {(status !== 'off') && <div className="timer">18:23</div>}
+        {(status === 'off') && <button className="btn">Start</button>}
+        {(status !== 'off') && <button className="btn">Stop</button>}
         <button className="btn btn-close">X</button>
       </div>
     )
-  }
+  };
 };
 
 render(<App />, document.querySelector('#app'));
